5eb9c48dcf18364676be6a6bd7219d5c9350cd3b
Isha Dijcks <ishadijcks@gmail.com>
2024-02-17 15:11:17 +0100

    Add prettier

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # NPM Package Template
 
 > A template to help you (me) quickly get started with publishing an NPM package
-
+>
+> Uses TypeScript, Vite, Vitest, Prettier
 
 ## Usage
 
-### Use as a template from GitHub
+### Create from a GitHub template
 
 ### Change these things
+
 - [ ] Update the Package name in `package.json`
 - [ ] Update the README with useful stuff :)
+- [ ] Update your Prettier settings and don't complain about my preferences :)
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,30 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite';
 
 // vite.config.js
-import dts from 'vite-plugin-dts'
-import tsconfigPaths from "vite-tsconfig-paths";
-// import pkg from './package.json' assert { type: 'json' }
+import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import pkg from './package.json' assert { type: 'json' };
 
 export default defineConfig({
-    build: {
-        lib: {
-            entry: 'src/index.ts',
-            fileName: "index.js",
-            formats: ['es'], // pure ESM package
-        },
-        rollupOptions: {
-            external: [
-                // ...Object.keys(pkg.dependencies), // don't bundle dependencies
-                /^node:.*/, // don't bundle built-in Node.js modules (use protocol imports!)
-            ],
-        },
-        target: 'esnext', // transpile as little as possible
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      fileName: 'index.js',
+      formats: ['es'], // pure ESM package
     },
+    rollupOptions: {
+      external: [
+        ...Object.keys(pkg.dependencies), // don't bundle dependencies
+        /^node:.*/, // don't bundle built-in Node.js modules (use protocol imports!)
+      ],
+    },
+    target: 'esnext', // transpile as little as possible
+  },
 
-    plugins: [
-        dts({
-            rollupTypes: true,
-        }),
-        tsconfigPaths(),
-    ],
-})
+  plugins: [
+    dts({
+      rollupTypes: true,
+    }),
+    tsconfigPaths(),
+  ],
+});
